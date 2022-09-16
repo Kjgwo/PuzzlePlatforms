97f305c0fb3db94559cf3d9f6e7abb93b8797414
김정우 <93897045+Kjgwo@users.noreply.github.com>
2022-09-16 15:41:07 +0900

    [ServerTravel] Hosting통해 서버 내에서 같은 Trigger 동작하기

--- Source/PuzzlePlatforms/PuzzlePlatformsGameInstance.cpp
+++ Source/PuzzlePlatforms/PuzzlePlatformsGameInstance.cpp
@@ -3,6 +3,8 @@
 
 #include "PuzzlePlatformsGameInstance.h"
 
+#include "Engine/Engine.h"
+
 UPuzzlePlatformsGameInstance::UPuzzlePlatformsGameInstance(const FObjectInitializer& ObjectInitializer)
 {
 	UE_LOG(LogTemp, Warning, TEXT("GameInstance Constructor"));
@@ -11,4 +13,27 @@ UPuzzlePlatformsGameInstance::UPuzzlePlatformsGameInstance(const FObjectInitiali
 void UPuzzlePlatformsGameInstance::Init()
 {
 	UE_LOG(LogTemp, Warning, TEXT("GameInstance Init"));
+}
+
+void UPuzzlePlatformsGameInstance::Host()
+{
+	UEngine* Engine = GetEngine();
+	if (!ensure(Engine != nullptr)) return;
+
+	
+	Engine->AddOnScreenDebugMessage(0, 2, FColor::Green, TEXT("Hosting"));
+
+	UWorld* World = GetWorld();
+	if (!ensure(World != nullptr)) return;
+
+	World->ServerTravel("/Game/ThirdPerson/Maps/ThirdPersonMap?listen");
+
+}
+
+void UPuzzlePlatformsGameInstance::Join(const FString& Address)
+{
+	UEngine* Engine = GetEngine();
+	if (!ensure(Engine != nullptr)) return;
+
+	Engine->AddOnScreenDebugMessage(0, 5, FColor::Green, FString::Printf(TEXT("Joining %s"), *Address));
 }
